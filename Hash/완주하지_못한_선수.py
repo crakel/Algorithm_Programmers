f1ee292f8b76b00f0295478b442f5cbea2d4ed65
김기윤 <crakel528@ajou.ac.kr>
2021-07-19 00:25:16 +0900

    Update Hash_1

--- Hash/완주하지_못한_선수.py
+++ Hash/완주하지_못한_선수.py
@@ -1,9 +1,8 @@
 def solution(participant, completion):
-    i = 0
-    while (len(participant) > 1):
-        if participant[i] in completion:
-            completion.remove(participant[i])
-            del participant[i]
-            i -= 1
-        i += 1
-    return participant.pop()
+    participant.sort()
+    completion.sort()
+
+    for p, c in zip(participant, completion):
+        if p != c:
+            return p
+    return participant[-1]
